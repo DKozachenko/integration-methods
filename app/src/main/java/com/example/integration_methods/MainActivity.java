2d9581290606c655d3c2248d57145bd0a30b0c1a
Diana <kozachenkodiana03@mail.ru>
2022-09-25 18:48:20 +0700

    Документирование

--- app/src/main/java/com/example/integration_methods/MainActivity.java
+++ app/src/main/java/com/example/integration_methods/MainActivity.java
@@ -24,14 +24,22 @@ public class MainActivity extends AppCompatActivity {
     private TextView rectangleOutput;
     private TextView trapezoidOutput;
     private TextView simpsonOutput;
-    private Integer START_SEGMENT_VALUE = 2;
 
+    /** Начальное значение кол-ва отрезков разбиения **/
+    private final Integer START_SEGMENT_VALUE = 2;
+
+    /** Округление
+     * @param value - значение
+     * @param digits - кол-во знаков после запятой
+     * @return округленное значение
+     **/
     private double roundToN(double value, int digits) {
         BigDecimal bd = new BigDecimal(Double.toString(value));
         bd = bd.setScale(digits, RoundingMode.HALF_UP);
         return bd.doubleValue();
     }
 
+    /** Инициализация переменных из разметки **/
     private void initializeVariables() {
         this.functionInput = findViewById(R.id.functionInput);
         this.leftLimitInput = findViewById(R.id.leftLimitInput);
@@ -42,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
         this.simpsonOutput = findViewById(R.id.simpsonOutput);
     }
 
+    /** Вычисление значения функции
+     * @param function - функция
+     * @param value - аргумент
+     * @return значение функции
+     **/
     private Double calculateFunctionValue(String function, Double value) {
         Context context = Context.enter();
         context.setOptimizationLevel(-1);
@@ -59,6 +72,11 @@ public class MainActivity extends AppCompatActivity {
         return Double.parseDouble(result);
     }
 
+    /** Одна итерация с помощью метода средних прямоугольников
+     * @param parameters - параметры
+     * @param segments - кол-во отрезков разбиения
+     * @return приближенное значение интеграла
+     **/
     private Double makeRectangleMethodIteration(Parameters parameters, Integer segments) {
         Double step = (parameters.getRightLimit() - parameters.getLeftLimit()) / segments;
 
@@ -75,6 +93,10 @@ public class MainActivity extends AppCompatActivity {
         return this.roundToN(sum * step, 5);
     }
 
+    /** Вычисление интеграла с определенной точностью с помощью метода средних прямоугольников
+     * @param parameters - параметры
+     * @return максимальное приближение интеграла с указанной точностью
+     **/
     private Double calculateByRectangleMethod(Parameters parameters) {
         Double lastApproximation = Double.MAX_VALUE;
         Integer segments = this.START_SEGMENT_VALUE;
@@ -89,6 +111,11 @@ public class MainActivity extends AppCompatActivity {
         return currentApproximation;
     }
 
+    /** Одна итерация с помощью метода средних трапеций
+     * @param parameters - параметры
+     * @param segments - кол-во отрезков разбиения
+     * @return приближенное значение интеграла
+     **/
     private Double makeTrapezoidMethodIteration(Parameters parameters, Integer segments) {
         Integer nodes = segments + 1;
         Double step = (parameters.getRightLimit() - parameters.getLeftLimit()) / segments;
@@ -102,12 +129,12 @@ public class MainActivity extends AppCompatActivity {
             sum += roundedResult;
         }
 
-        //первое значение функ
+        //первое значение функции
         Double x0 = parameters.getLeftLimit() + 0 * step;
         Double resultFirstNodeValue = this.calculateFunctionValue(parameters.getFunction(), x0);
         Double roundedFirstNodeValue = roundToN(resultFirstNodeValue, parameters.getRoundNumber());
 
-        //последнее значение функ
+        //последнее значение функции
         Double x2n = parameters.getLeftLimit() + (nodes - 1) * step;
         Double resultLastNodeValue = this.calculateFunctionValue(parameters.getFunction(), x2n);
         Double roundedLastNodeValue = roundToN(resultLastNodeValue, parameters.getRoundNumber());
@@ -117,6 +144,10 @@ public class MainActivity extends AppCompatActivity {
         return this.roundToN(sum * step, 5);
     }
 
+    /** Вычисление интеграла с определенной точностью с помощью метода трапеций
+     * @param parameters - параметры
+     * @return максимальное приближение интеграла с указанной точностью
+     **/
     private Double calculateByTrapezoidMethod(Parameters parameters) {
         Double lastApproximation = Double.MAX_VALUE;
         Integer segments = this.START_SEGMENT_VALUE;
@@ -131,6 +162,11 @@ public class MainActivity extends AppCompatActivity {
         return currentApproximation;
     }
 
+    /** Одна итерация с помощью метода Симпсона
+     * @param parameters - параметры
+     * @param segments - кол-во отрезков разбиения
+     * @return приближенное значение интеграла
+     **/
     private Double makeSimpsonMethodIteration(Parameters parameters, Integer segments) {
         Integer nodes = segments + 1;
         Double step = (parameters.getRightLimit() - parameters.getLeftLimit()) / segments;
@@ -153,14 +189,14 @@ public class MainActivity extends AppCompatActivity {
 
         sum += sumEven * 2 + sumOdd * 4;
 
-        //первое значение функ
+        //первое значение функции
         Double x0 = parameters.getLeftLimit() + 0 * step;
         Double resultFirstNodeValue = this.calculateFunctionValue(parameters.getFunction(), x0);
         Double roundedFirstNodeValue = roundToN(resultFirstNodeValue, parameters.getRoundNumber());
 
         sum += roundedFirstNodeValue;
 
-        //последнее значение функ
+        //последнее значение функции
         Double x2n = parameters.getLeftLimit() + (nodes - 1) * step;
         Double resultLastNodeValue = this.calculateFunctionValue(parameters.getFunction(), x2n);
         Double roundedLastNodeValue = roundToN(resultLastNodeValue, parameters.getRoundNumber());
@@ -170,6 +206,10 @@ public class MainActivity extends AppCompatActivity {
         return this.roundToN(sum * (step / 3), 5);
     }
 
+    /** Вычисление интеграла с определенной точностью с помощью метода Симпсона
+     * @param parameters - параметры
+     * @return максимальное приближение интеграла с указанной точностью
+     **/
     private Double calculateBySimpsonMethod(Parameters parameters) {
         Double lastApproximation = Double.MAX_VALUE;
         Integer segments = this.START_SEGMENT_VALUE;
@@ -184,6 +224,9 @@ public class MainActivity extends AppCompatActivity {
         return currentApproximation;
     }
 
+    /** Сбор параметров в экземпляр класса
+     * @return экзмепляр класса Parameters
+     **/
     private Parameters getParameters() {
         String initialFunction = this.functionInput.getText().toString();
         String function = this.editInitialFunction(initialFunction);
@@ -196,11 +239,18 @@ public class MainActivity extends AppCompatActivity {
         return new Parameters(function, leftLimit, rightLimit, roundNumber, accuracy);
     }
 
+    /** Вывод сообщения на экран
+     * @param message - сообщение
+     **/
     private void showMessage(String message) {
         Toast toast = Toast.makeText(getApplicationContext(),message, Toast.LENGTH_SHORT);
         toast.show();
     }
 
+    /** Подсчет открывающих круглых скобочек
+     * @param function - функция
+     * @return кол-во открывающих круглых скобочек
+     **/
     private Integer countOpenParentheses(String function) {
         Integer count = 0;
 
@@ -213,6 +263,10 @@ public class MainActivity extends AppCompatActivity {
         return count;
     }
 
+    /** Подсчет закрывающих круглых скобочек
+     * @param function - функция
+     * @return кол-во закрывающих круглых скобочек
+     **/
     private Integer countCloseParentheses(String function) {
         Integer count = 0;
 
@@ -225,6 +279,9 @@ public class MainActivity extends AppCompatActivity {
         return count;
     }
 
+    /** Проверка валидности введенной функции
+     * @return валидна / не валидна
+     **/
     private Boolean isInitialFunctionDataValid() {
         String initialFunction = this.functionInput.getText().toString();
         if (initialFunction.length() < 1) {
@@ -260,6 +317,9 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /** Проверка валидности левой границы
+     * @return валидна / не валидна
+     **/
     private Boolean isInitialLeftLimitValid(String leftLimit) {
         if (leftLimit.length() < 1) {
             this.showMessage("Левая граница не может быть пустая");
@@ -274,6 +334,9 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /** Проверка валидности правой границы
+     * @return валидна / не валидна
+     **/
     private Boolean isInitialRightLimitValid(String rightLimit) {
         if (rightLimit.length() < 1) {
             this.showMessage("Правая граница не может быть пустая");
@@ -288,6 +351,9 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /** Проверка валидности округления
+     * @return валидно / не валидно
+     **/
     private Boolean isInitialRoundValid() {
         String initialRound = this.roundNumberInput.getText().toString();
 
@@ -309,6 +375,9 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /** Проверка валидности промежутка
+     * @return валиден / не валиден
+     **/
     private Boolean isIntervalValid(Double leftLimit, Double rightLimit) {
         if (leftLimit >= rightLimit) {
             this.showMessage("Левая граница больше или равна правой");
@@ -318,6 +387,9 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /** Проверка валидности введнных значений
+     * @return валидны / не валидны
+     **/
     private Boolean isDataValid() {
         String initialLeftLimit = this.leftLimitInput.getText().toString();
         String initialRightLimit = this.rightLimitInput.getText().toString();
@@ -329,6 +401,9 @@ public class MainActivity extends AppCompatActivity {
                 && this.isIntervalValid(Double.parseDouble(initialLeftLimit), Double.parseDouble(initialRightLimit));
     }
 
+    /** Парсинг функции натурального логарифма для движка JS
+     * @return обработанная функция
+     **/
     private String parseLnFunction(String function) {
         String parsedFunction = function;
         while (parsedFunction.contains("ln")) {
@@ -338,6 +413,9 @@ public class MainActivity extends AppCompatActivity {
         return parsedFunction;
     }
 
+    /** Парсинг функции десятичного логарифма для движка JS
+     * @return обработанная функция
+     **/
     private String parseLgFunction(String function) {
         String parsedFunction = function;
         while (parsedFunction.contains("lg")) {
@@ -347,6 +425,9 @@ public class MainActivity extends AppCompatActivity {
         return parsedFunction;
     }
 
+    /** Парсинг функции квадратного корня для движка JS
+     * @return обработанная функция
+     **/
     private String parseSqrtFunction(String function) {
         String parsedFunction = function;
 
@@ -363,6 +444,9 @@ public class MainActivity extends AppCompatActivity {
         return parsedFunction;
     }
 
+    /** Парсинг функции синуса для движка JS
+     * @return обработанная функция
+     **/
     private String parseSinFunction(String function) {
         String parsedFunction = function;
 
@@ -379,6 +463,9 @@ public class MainActivity extends AppCompatActivity {
         return parsedFunction;
     }
 
+    /** Парсинг функции косинуса для движка JS
+     * @return обработанная функция
+     **/
     private String parseCosFunction(String function) {
         String parsedFunction = function;
 
@@ -395,6 +482,9 @@ public class MainActivity extends AppCompatActivity {
         return parsedFunction;
     }
 
+    /** Парсинг функции модуля для движка JS
+     * @return обработанная функция
+     **/
     private String parseAbsFunction(String function) {
         String parsedFunction = function;
 
@@ -415,6 +505,9 @@ public class MainActivity extends AppCompatActivity {
         return parsedFunction;
     }
 
+    /** Парсинг степенной функции для движка JS
+     * @return обработанная функция
+     **/
     private String parsePowFunction(String function) {
         String parsedFunction = function;
 
@@ -469,7 +562,9 @@ public class MainActivity extends AppCompatActivity {
         return parsedFunction;
     }
 
-
+    /** Парсинг функции для движка JS
+     * @return конечная обработанная функция для JS
+     **/
     private String editInitialFunction(String initialFunction) {
         String finalFunction = initialFunction;
 
@@ -492,6 +587,7 @@ public class MainActivity extends AppCompatActivity {
         this.initializeVariables();
     }
 
+    /** Обработка нажатия на кнопку "Метод прямоугольников" **/
     public void rectangleButtonClick(View view) {
         if (this.isDataValid()) {
             Parameters parameters = this.getParameters();
@@ -500,6 +596,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /** Обработка нажатия на кнопку "Метод трапеций" **/
     public void trapezoidButtonClick(View view) {
         if (this.isDataValid()) {
             Parameters parameters = this.getParameters();
@@ -508,6 +605,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /** Обработка нажатия на кнопку "Метод Симпсона" **/
     public void simpsonButtonClick(View view) {
         if (this.isDataValid()) {
             Parameters parameters = this.getParameters();
--- app/src/main/java/com/example/integration_methods/Parameters.java
+++ app/src/main/java/com/example/integration_methods/Parameters.java
@@ -1,10 +1,16 @@
 package com.example.integration_methods;
 
+/**Ккласс параметров **/
 public class Parameters {
+    /** Функция **/
     private String function;
+    /** Левая граница **/
     private Double leftLimit;
+    /** Правая граница **/
     private Double rightLimit;
+    /** Кол-во знаков после запятой **/
     private Integer roundNumber;
+    /** Точность **/
     private Double accuracy;
 
     public Parameters(String function, Double leftLimit, Double rightLimit, Integer roundNumber, Double accuracy) {
@@ -15,22 +21,27 @@ public class Parameters {
         this.accuracy = accuracy;
     }
 
+    /** Геттер для функции **/
     public String getFunction() {
         return function;
     }
 
+    /** Геттер для левой границы **/
     public Double getLeftLimit() {
         return leftLimit;
     }
 
+    /** Геттер для правой границы **/
     public Double getRightLimit() {
         return rightLimit;
     }
 
+    /** Геттер для кол-ва цифр после запятой **/
     public Integer getRoundNumber() {
         return roundNumber;
     }
 
+    /** Геттер для точности **/
     public Double getAccuracy() {
         return accuracy;
     }
